21e4a33d4240b46228d33c59c49b05e9db70ed9c
JohnDoddy <johndoddy99@gmail.com>
2019-07-25 21:01:56 +0300

    Add a challenge

---
+++ 15_divisor2.py
@@ -0,0 +1,16 @@
+# Create a function named divisors/Divisors that takes an integer
+# n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), 
+# from smallest to largest. If the number is prime return the string '(integer) is prime'
+# 
+
+def divisor(integer):
+    arr = []
+    for x in range(2,integer - 1): #if integer is 12
+        if integer % x == 0:
+            arr.append(x)
+    if len(arr) == 0:
+        print(integer, 'is prime')
+    else:
+        print(arr)
+
+divisor(12)
