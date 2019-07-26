5e42209027ace85832ec86c849e7e961ed6df963
JohnDoddy <johndoddy99@gmail.com>
2019-07-26 21:28:46 +0300

    Add a challenge

---
+++ 17_dictionaries.py
@@ -0,0 +1,14 @@
+# a dictionary/ hashtable
+values = {"name":"John", "age": 19}
+
+# iterating over a dictionary
+for key, value in values.items():
+    print("My {} is {}".format(key, value))
+
+# updating dictionary values
+values["name"] = "Doddy"
+print(values["name"])
+
+# deleting dictionary items
+values.__delitem__("name")
+print(values)
