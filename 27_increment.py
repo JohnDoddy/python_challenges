9fa6eeb8c1093924999f25192a99a9a1b79d1915
JohnDoddy <johndoddy99@gmail.com>
2019-08-10 14:46:22 +0300

    Add a challenge

---
+++ 27_increment.py
@@ -0,0 +1,5 @@
+number = int(input("What is the number you want to increase?: "))
+
+def increment(number, by=1):
+	print(number + by)
+increment()
---
+++ 28_number_is_prime.py
@@ -0,0 +1,19 @@
+# prime number is only divisible by 1 and itself
+import time  # test how fast this implementation is 
+
+def prime(number):
+    """
+    Return true if a 
+    number is prime
+    """
+    if number == 1:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+#test
+
+for n in range(1, 21):
+    print(n, prime(n))
